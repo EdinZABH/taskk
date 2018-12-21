f95c7d4886475fcdccafd3ecd1344d517928bec7
Edin Zagora <edin.zagora@atlantbh.com>
2018-12-21 15:52:29 +0100

    modified:   deploy_wordpress.sh

--- deploy_wordpress.sh
+++ deploy_wordpress.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=sifra -d mysql:5.7
-sudo sleep 60
 sudo docker run --name some-wordpress --link some-mysql:mysql -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=sifra -p 80:80 -d wordpress
