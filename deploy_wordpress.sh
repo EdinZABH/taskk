71bb4f6d38a32ef6595c49b756cc454b079893a1
Edin Zagora <edin.zagora@atlantbh.com>
2019-01-09 09:41:48 +0100

    deleted:    README.md 	deleted:    deploy_wordpress.sh

--- deploy_wordpress.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=sifra -d mysql:5.7
-sudo docker run --name some-wordpress --link some-mysql:mysql -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=sifra -p 80:80 -d wordpress
